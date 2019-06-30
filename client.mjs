cb9b2cb981a492a840fb8773423ef04a6024b06c
Karolis Kraujelis <kudziokaschannel@gmail.com>
2019-07-01 01:39:33 +0300

    Core code layout finished (untested)

---
+++ client.mjs
@@ -0,0 +1,133 @@
+import alt from "alt";
+import game from "natives";
+
+let TEXT3D_LIST = [];
+
+let findUnusedText3DID = () => {
+    for(let i = 0; i < TEXT3D_LIST.length; i++)
+        if(TEXT3D_LIST[i] === undefined) return i;
+
+    return TEXT3D_LIST.length;
+};
+
+/**
+ * Function is borrowed & edited from: https://forum.altv.mp/index.php?/topic/33-basic-3dtext/
+ * 
+ * @param x 
+ * @param y 
+ * @param z 
+ * @param name 
+ */
+let draw3dText = (text3D) => {
+    let pos = text3D.getPosition();
+
+    const [bol, _x, _y] = game.getScreenCoordFromWorldCoord(pos.x, pos.y, pos.z);
+    const camCord = game.getGameplayCamCoords();
+    const dist = game.getDistanceBetweenCoords(camCord.x,camCord.y,camCord.z, pos.x, pos.y, pos.z, 1);
+
+    if (dist > text3D.maxRenderDist) return;
+
+    let scale = (4.00001/dist) * 0.3 * text3D.scale;
+    if (scale > 0.2)
+        scale = 0.2;
+
+    const fov = (1/game.getGameplayCamFov())*100;
+	scale = scale*fov;
+  
+    if (bol){
+        game.setTextScale(scale, scale);
+        game.setTextFont(text3D.font);
+        game.setTextProportional(true);
+        game.setTextColour(text3D.color[0], text3D.color[1], text3D.color[2], text3D.color[3]);
+        game.setTextDropshadow(0, 0, 0, 0, 255);
+        game.setTextEdge(2, 0, 0, 0, 150);
+        game.setTextDropShadow();
+        game.setTextOutline();
+        game.setTextCentre(text3D.centre);
+        game.beginTextCommandDisplayText("STRING");
+        game.addTextComponentSubstringPlayerName(text3D.text);
+        game.endTextCommandDisplayText(_x,_y + 0.025);
+    }
+
+}
+
+export default {
+    /**
+     * @param text - STRING text (maxlen: 256);
+     * @param pos - OBJECT {x,y,z} pos || FUNCTION pos;
+     * @param options - OBJECT {dynamic} options;
+     */
+    new: (text, pos, {font = 0, color = [255, 255, 255, 255], centre = true, scale = 1, maxRenderDist = 20}) => {
+        let id = findUnusedText3DID();
+
+        TEXT3D_LIST[id] = {
+            text: text,
+            pos: pos,
+            font: font,
+            color: color,
+            center: centre,
+            scale: scale,
+            maxRenderDist: maxRenderDist,
+            getPosition: () => { 
+                const posType = typeof this.pos;
+
+                if(posType === "function")
+                    return this.pos();
+
+                if(posType === "object") {
+                    if(this.pos.position !== undefined)
+                        return this.pos.position;
+
+                    if(this.pos.pos !== undefined)
+                        return this.pos.pos;
+
+                    return this.pos;
+                }
+
+                else alt.warn("altmp-js-3dtext-spawner unable to decypher this.pos in getPosition()");
+
+                return {
+                    x: 0,
+                    y: 0,
+                    z: 0
+                };
+            }
+        };
+
+        return {
+            id: id,
+            destroy: () => {
+                PROPS_LIST[this.id] = undefined;
+            },
+
+            get position() {
+                return PROPS_LIST[this.id].getPosition();
+            },
+            set position(position) {
+                PROPS_LIST[this.id].pos = position;
+            },
+
+            get text() {
+                return PROPS_LIST[this.id].text;
+            },
+            set text(text) {
+                PROPS_LIST[this.id].text = text;
+            }
+        };
+    }
+};
+
+alt.on(
+    'update',
+    () => {
+        TEXT3D_LIST.forEach(
+            (text3D) => {
+                if(text3D !== undefined) {
+                    draw3dText(
+                        text3D
+                    );
+                }
+            }
+        )
+    }
+)
